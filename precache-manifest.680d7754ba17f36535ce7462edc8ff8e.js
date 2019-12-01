64cd578ca5a583a3e89ac3326839486be35d0970
realrlgus <rkgus102@gmail.com>
2019-12-01 23:01:07 +0900

    Updates

---
+++ precache-manifest.680d7754ba17f36535ce7462edc8ff8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4543cb88ddd04a4304fbd3f44248ed2e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d0f12ac72c9ad566efab",
+    "url": "/movie_app/static/css/main.2496ae72.chunk.css"
+  },
+  {
+    "revision": "e183bcee449302b2a05c",
+    "url": "/movie_app/static/js/2.6196da2f.chunk.js"
+  },
+  {
+    "revision": "d0f12ac72c9ad566efab",
+    "url": "/movie_app/static/js/main.e73ea23a.chunk.js"
+  },
+  {
+    "revision": "d179891a5f5e4ba41521",
+    "url": "/movie_app/static/js/runtime~main.76fa3daa.js"
+  }
+]);
